760ac11f914a84a2c26eacae97e02ed268d374ba
Samuel Duhaime-Morissette <sduhaimemorissette@gmail.com>
2025-05-19 17:47:36 -0400

    Add Accounts data and routes
    For now, the Accounts data are without a database.
    Test the API Accounts routes with pytest.
    Update the workflows and tasks.json with that.
    Install all the dependencies needed for this.

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 
+from src.features.accounts.routes import accounts_router
+
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(accounts_router, prefix="/accounts")
 
 
 def start():
